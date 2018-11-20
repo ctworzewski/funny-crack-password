66eeebcc9cc1edb727a0ec01dfc3d7b32cea5f12
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2018-11-21 00:45:32 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const input = document.getElementById('pass');
+const div = document.querySelector('.message');
+
+const password = 'ciasteczko';
+const message = 'Brawo! Każdy je lubi';
+
+
+input.addEventListener('input', (e) => {
+    if (password === e.target.value) {
+        div.textContent = message;
+    }
+})
